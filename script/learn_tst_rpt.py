80c90ff0a2340a95974afdc0c4e7f7a58d1b1ae0
Dan Bikle <bikle@bikle.com>
2016-10-03 02:59:25 -0700

    more

--- script/learn_tst_rpt.py
+++ script/learn_tst_rpt.py
@@ -75,7 +75,37 @@ sess = tf.InteractiveSession()
 
 # tf wants class training values to be 1 hot encoded.
 class_train1h_l = [[0,1] if cl else [1,0] for cl in class_train_a]
-class_train1h_a = np.array(class_train1h_l)
+# [0,1] means up-observation
+# [1,0] means down-observation
+ytrain1h_a = np.array(class_train1h_l)
+learning_rate   = 0.001
+
+
+# I declare 2d Tensors.
+# I should use 0th row of x_train_a to help shape xvals:
+fnum_i  = len(x_train_a[0, :])
+label_i = len(ytrain1h_a[0,:]) # Should usually be 2.
+
+print(str(yr)+' VERY Busy...')
+xvals = tf.placeholder(tf.float32, shape=[None, fnum_i], name='x-input')
+weight1 = tf.Variable(tf.zeros([fnum_i, label_i]))
+weight0 = tf.Variable(tf.zeros([label_i]))
+yhat    = tf.nn.softmax(tf.matmul(xvals, weight1) + weight0)
+# Define loss and optimizer
+yactual       = tf.placeholder(tf.float32, [None, label_i])
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(yactual * tf.log(yhat), reduction_indices=[1]))
+# http://www.google.com/search?q=tensorflow+GradientDescentOptimizer+vs+AdamOptimizer
+# http://sebastianruder.com/optimizing-gradient-descent/
+#train_step    = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+train_step    = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
+# Train
+training_steps_i = 77
+tf.initialize_all_variables().run()
+for i in range(training_steps_i):
+  train_step.run({xvals: x_train_a, yactual: ytrain1h_a})
+prob_a = sess.run(yhat, feed_dict={xvals: x_test_a})
+
+
 
 pdb.set_trace()
 # I should create a CSV to report from:
