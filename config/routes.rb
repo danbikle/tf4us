c209d66e61b2a5020bf53b1f5aa1727bd9d5236c
Dan Bikle <bikle@bikle.com>
2016-10-12 17:14:00 -0700

    more

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
-  get 'pages/about'
-
-  get 'pages/blog'
-
-  get 'pages/compare'
-
-  get 'pages/contact'
-
-  get 'pages/whatif'
-
-  get 'pages/backtests'
-
+  get  'pages/about'
+  get  'pages/blog'
+  get  'pages/compare'
+  get  'pages/contact'
+  get  'pages/whatif'
+  root 'pages#home'
+  get  'pages/backtests'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
